4a4f0d7ab52ca4e79b863f95aa2ae40021bf734b
TerrellAW <terraw1999@proton.me>
2025-10-29 20:20:23 -0600

    Plan: Scaffolding source files

---
+++ src/font.c
@@ -0,0 +1,32 @@
+#include "../include/glyph.h"
+#include "../include/font.h"
+
+// Function declarations
+int compute_ascent(Glyph* glyphs, size_t count);
+int compute_descent(Glyph* glyphs, size_t count);
+
+// Function implementations
+
+Font font_create(Glyph* glyphs, size_t count) {
+	Font font;
+	font.glyphs = glyphs;
+	font.glyph_count = count;
+	font.em_size = 1000;
+	font.name = "DigitalSegmented";
+	font.ascent = compute_ascent(glyphs, count);
+	font.descent = compute_descent(glyphs, count);
+	return font;
+}
+
+int compute_ascent(Glyph* glyphs, size_t count) {
+	
+}
+
+int compute_descent(Glyph* glyphs, size_t count) {
+	
+}
+
+void font_free(Font font) {
+	
+}
+
