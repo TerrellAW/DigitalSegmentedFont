a0718b8394f10f816eeadbc831dc020d640dea8e
TerrellAW <terraw1999@proton.me>
2025-11-03 21:09:26 -0700

    Add: Implementation for glyphs_create()

--- src/font.c
+++ src/font.c
@@ -1,38 +1,64 @@
+#include "../include/utils.h"
 #include "../include/glyph.h"
 #include "../include/font.h"
 
-// Function declarations
-int compute_ascent(Glyph* glyphs, size_t count);
-int compute_descent(Glyph* glyphs, size_t count);
-
-Font font_create(Glyph* glyphs, size_t count) {
+// Initialize font
+Font font_create() {
 	Font font;
-	font.glyphs = glyphs;
-	font.glyph_count = count;
+	font.glyphs = NULL;
+	font.glyph_count = 0;
 	font.em_size = 1000;
 	font.name = "DigitalSegmented";
-	font.ascent = compute_ascent(glyphs, count);
-	font.descent = compute_descent(glyphs, count);
+	font.ascent = 0;
+	font.descent = 0;
 	return font;
 }
 
+// Create array of Glyph structs
+Glyph* glyphs_create(size_t* out_count) {
+	// Define range of Unicode codepoints
+	const uint32_t start = 0x20; // Space
+	const uint32_t end	 = 0x7E; // Tilde
+
+	size_t count = end - start + 1; // Adding 1 ensures inclusive
+	if (out_count) *out_count = count; // Optional external pointer
+
+	// Allocate memory for glyph array
+	Glyph* glyphs = safe_malloc(count * sizeof(Glyph));
+
+	// Initialize each glyph
+	for (size_t i = 0; i < count; i++) {
+		uint32_t codepoint = start + 1;
+		glyphs[i] = glyph_create(codepoint);
+	}
+
+	return glyphs;
+}
+
+// Add glyph to font
+int font_add_glyphs(Font* font, Glyph* glyph) {
+	
+}
+
+// Compute max height of glyphs
 int compute_ascent(Glyph* glyphs, size_t count) {
-	int y_max = 0; // Temp
 	
-	// TODO: Loop through glyphs and find highest vertical coord
-	
-	return y_max;
 }
 
+// Compute max depth of glyphs
 int compute_descent(Glyph* glyphs, size_t count) {
-	int y_min = 0; // Temp
 	
-	// TODO: Loop through glyphs and find the lowest vertical coord
-	
-	return y_min;
 }
 
-void font_free(Font font) {
-	// TODO: Free memory
+// Release memory used to create Font
+void font_free(Font* font) {
+	/* TODO: Adapt code for font
+	if (!glyphs) return; // Check if array exists
+	for (size_t i = 0; i < count; i++) { // Iterate through array of glyphs
+		glyph_free(&glyphs[i]); // Send memory address pointer to glyph_free()
+	}
+	free(glyphs); // Release memory holding array
+	glyphs = NULL;
+	*/
 }
 
